3286f5439ca2d40c82d6d190a3c52545031964f9
F4tsk4l <enockkip11@gmail.com>
2024-06-07 11:26:21 +0300

    Lsp configured

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -1,81 +1,75 @@
-require("cmp_git").setup()
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_format = require('lsp-zero').cmp_format({details = true})
 cmp.setup({
-snippet = {
-  -- REQUIRED - you must specify a snippet engine
-  expand = function(args)
-    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-   require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-  end,
-},
-window = {
-  completion = cmp.config.window.bordered(),
-  documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
-  ['<C-y>'] = cmp.mapping.confirm({select = false}),
-  ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-  ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<M-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-}),
-sources = cmp.config.sources({
-  { name = 'nvim_lsp' },
-  -- { name = 'vsnip' }, -- For vsnip users.
-  { name = 'luasnip' }, -- For luasnip users.
-  -- { name = 'ultisnips' }, -- For ultisnips users.
-  -- { name = 'snippy' }, -- For snippy users.
-}, {
-  { name = 'buffer' },
-  { name = 'path' },
-})
-})
+  sources = {
+    {name = 'nvim_lsp'},
+    {name = 'luasnip'},
+    {name = 'buffer'},
+    {name = 'path'},
+    {name = 'nvim_lua'},
+  },
+   preselect = 'item',
+  completion = {
+  completeopt = 'menu,menuone,noinsert'
+  },
+   window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = {
+    --['<Tab>'] = cmp_action.tab_complete(),
+    --['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    ['<C-j>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-k>'] = cmp_action.luasnip_jump_backward(),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<M-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = false}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+--- (Optional) Show source name in completion menu
+--  formatting = cmp_format,
+  formatting = {
+    -- changing the order of fields so the icon is the first
+    fields = {'menu', 'abbr', 'kind'},
 
-cmp.setup.cmdline(':', {
-mapping = cmp.mapping.preset.cmdline(),
-sources = cmp.config.sources({
-  { name = 'path' }
-}, {
-  { name = 'cmdline' }
-}),
-matching = { disallow_symbol_nonprefix_matching = false }
-})
+    -- here is where the change happens
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = 'λ',
+        luasnip = '⋗',
+        buffer = 'Ω',
+        path = '🖫',
+        nvim_lua = 'Π',
+      }
 
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
-  { name = 'git' },
-}, {
-  { name = 'buffer' },
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
+  },
 })
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-mapping = cmp.mapping.preset.cmdline(),
-sources = {
-  { name = 'buffer' }
-}
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['lua_ls'].setup {
-capabilities = capabilities
-}
-require('lspconfig')['clangd'].setup {
-capabilities = capabilities
-}
-require('lspconfig')['jedi_language_server'].setup {
-capabilities = capabilities
-}
--- lua/fconfig/plugins.lua
+++ lua/fconfig/plugins.lua
@@ -3,57 +3,58 @@
 local cmd = require('pckr.loader.cmd')
 local keys = require('pckr.loader.keys')
 
-require('pckr').add{
-  { 'rose-pine/neovim' };
-  { 'catppuccin/nvim' };
-  { 'philrunninger/nerdtree-visual-selection' };
-  { 'christoomey/vim-tmux-navigator' };
-  { 'mbbill/undotree' };
-  { 'preservim/nerdtree' };
-  { 'lambdalisue/suda.vim' };
-  { 'folke/trouble.nvim' };
-  { 'folke/todo-comments.nvim' };
-  { 'ryanoasis/vim-devicons' };
-  { 'Xuyuanp/nerdtree-git-plugin' };
-  { 'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true}
-    };
-  { 'tiagofumo/vim-nerdtree-syntax-highlight' };
-  { 'Mofiqul/dracula.nvim' };
-  { 'norcalli/nvim-colorizer.lua' };
-  { 'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = function()
-        require("nvim-autopairs").setup {}
-    end
-    };
-  { 'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    requires = {'nvim-lua/plenary.nvim'}
-    };
-  { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' };
-  { 'stevearc/dressing.nvim' };
-  { 'petertriho/cmp-git' };
-  { 'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+require('pckr').add {
+    { 'rose-pine/neovim' },
+    { 'catppuccin/nvim' },
+    { 'philrunninger/nerdtree-visual-selection' },
+    { 'christoomey/vim-tmux-navigator' },
+    { 'mbbill/undotree' },
+    { 'preservim/nerdtree' },
+    { 'lambdalisue/suda.vim' },
+    { 'folke/trouble.nvim' },
+    { 'folke/todo-comments.nvim' },
+    { 'ryanoasis/vim-devicons' },
+    { 'Xuyuanp/nerdtree-git-plugin' },
+    { 'lukas-reineke/lsp-format.nvim' },
+    { 'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    },
+    { 'tiagofumo/vim-nerdtree-syntax-highlight' },
+    { 'Mofiqul/dracula.nvim' },
+    { 'norcalli/nvim-colorizer.lua' },
+    { 'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    },
+    { 'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        requires = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+    { 'stevearc/dressing.nvim' },
+    { 'petertriho/cmp-git' },
+    { 'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  };
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    },
 
 }
