ae93ca9df26e00b411ced4eb8ceedfdc6c1d4481
F4tsk4l <enockkip11@gmail.com>
2024-10-09 22:48:41 +0300

    Todo comment keybindings set

--- lua/fconfig/set.lua
+++ lua/fconfig/set.lua
@@ -3,7 +3,6 @@ vim.opt.guicursor = ""
 if vim.fn.has("termguicolors") then
     vim.o.termguicolors = true
 end
-
 if vim.fn.has('nvim') then
     vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
 end
@@ -11,33 +10,31 @@ vim.g.NERDTreeGitStatusUseNerdFonts = 1
 vim.g.NERDTreeGitStatusUntrackedFilesMode = 'all'
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.spell.spelllang = 'en_us'
+vim.opt.spell = true
 -- Color schemes
---vim.cmd.colorscheme "catppuccin"
 vim.cmd("colorscheme rose-pine")
---vim.cmd[[colorscheme dracula]]
---vim.cmd[[colorscheme dracula-soft]]
---TODO:
+-- TODO: Test
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 ----------
---TODO:-COMMENTS KEYBINDINGS
-vim.keymap.set("n", "]t", function()
-    require("todo-comments").jump_next()
-end, { desc = "Next todo comment" })
-
-vim.keymap.set("n", "]t", function()
-    require("todo-comments").jump_prev()
-end, { desc = "Previous todo comment" })
-
+-- TODO: Keybindings
+--
+--vim.keymap.set("n", "]t", function()
+--    require("todo-comments").jump_next()
+--end, { desc = "Next todo comment" })
+--
+--vim.keymap.set("n", "[t", function()
+--    require("todo-comments").jump_prev()
+--end, { desc = "Previous todo comment" })
+-- WARNING: Test
 -- You can also specify a list of valid jump keywords
 
-vim.keymap.set("n", "]t", function()
-    require("todo-comments").jump_next({ keywords = { "ERROR", "WARNING" } })
-end, { desc = "Next error/warning todo comment" })
+--vim.keymap.set("n", "]t", function()
+--    require("todo-comments").juWp_next({ keywords = { "ERROR", "WARNING" } })
+--end, { desc = "Next error/warning todo comment" })
 -----------
 vim.opt.smartindent = true
 
