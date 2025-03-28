8a1a85d402acecadb30b5084a758bbf4c751d703
F4tsk4l <enockkip11@gmail.com>
2025-03-28 17:29:56 +0300

    vimwiki stuff added but not working yet

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -7,11 +7,17 @@ return {
       syntax = "markdown",
       ext = ".md",
     }
+    --    vim.g.vimwiki_list = {
+    --      path = "~/Desktop/Mu5tur4d/N0t35/vimwiki/",
+    --      syntax = "markdown",
+    --      ext = ".md",
+    --    }
     vim.g.vimwiki_ext2syntax = {
       [".md"] = "markdown",
       [".markdown"] = "markdown",
       [".mdown"] = "markdown",
     }
+    vim.g.vimwiki_listsyms = "✗○◐●✓"
     vim.g.vimwiki_global_ext = 0 -- don't treat all md files as vimwiki (0)
     vim.g.vimwiki_hl_headers = 1 -- use alternating colours for different heading levels
     vim.g.vimwiki_markdown_link_ext = 1 -- add markdown file extension when generating links
