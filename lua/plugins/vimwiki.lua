9483906caafd29257a113921cb3820ff9ce1c6a7
F4tsk4l <enockkip11@gmail.com>
2025-03-28 16:08:44 +0300

    vimwiki added

---
+++ lua/plugins/vimwiki.lua
@@ -0,0 +1,20 @@
+return {
+  "vimwiki/vimwiki",
+  config = function()
+    --opts = {
+    vim.g.vimwiki_list = {
+      path = "~/vimwiki/",
+      syntax = "markdown",
+      ext = ".md",
+    }
+    vim.g.vimwiki_ext2syntax = {
+      [".md"] = "markdown",
+      [".markdown"] = "markdown",
+      [".mdown"] = "markdown",
+    }
+    vim.g.vimwiki_global_ext = 0 -- don't treat all md files as vimwiki (0)
+    vim.g.vimwiki_hl_headers = 1 -- use alternating colours for different heading levels
+    vim.g.vimwiki_markdown_link_ext = 1 -- add markdown file extension when generating links
+    vim.g.taskwiki_markdown_syntax = "markdown"
+  end,
+}
