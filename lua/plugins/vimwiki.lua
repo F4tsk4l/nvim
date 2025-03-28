9d7ddc9a0a3b8821bbf1eb4657b952a1a4111707
F4tsk4l <enockkip11@gmail.com>
2025-03-28 21:44:35 +0300

    vimwiki back to wiki ext.

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -4,20 +4,22 @@ return {
     vim.g.vimwiki_list = {
       {
         path = "/home/g4m3r/vimwiki/",
-        syntax = "markdown",
-        ext = ".md",
+        syntax = "default",
+        -- ext = ".md",
+        ext = ".wiki",
       },
       {
         path = "/home/g4m3r/Desktop/Mu5tur4d/N0t35/vimwiki/",
-        syntax = "markdown",
-        ext = ".md",
+        syntax = "default",
+        --ext = ".md",
+        ext = ".wiki",
       },
     }
-    vim.g.vimwiki_ext2syntax = {
-      [".md"] = "markdown",
-      [".markdown"] = "markdown",
-      [".mdown"] = "markdown",
-    }
+    --    vim.g.vimwiki_ext2syntax = {
+    --      [".md"] = "markdown",
+    --      [".markdown"] = "markdown",
+    --      [".mdown"] = "markdown",
+    --    }
     vim.g.vimwiki_listsyms = "✗○◐●✓"
     vim.g.vimwiki_global_ext = 0 -- don't treat all md files as vimwiki (0)
     vim.g.vimwiki_hl_headers = 1 -- use alternating colours for different heading levels
